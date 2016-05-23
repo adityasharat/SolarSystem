1e96ae3f3ac6dce9e0156574db04891e19991bd4
Aditya Sharat <aditya.sharat@flipkart.com>
2016-05-23 18:31:46 +0530

    changes

--- app/src/main/java/com/jsvoid/solarsystem/activities/ListActivity.java
+++ app/src/main/java/com/jsvoid/solarsystem/activities/ListActivity.java
@@ -3,12 +3,14 @@ package com.jsvoid.solarsystem.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.jsvoid.solarsystem.R;
 import com.jsvoid.solarsystem.SolarSystemApplication;
+import com.jsvoid.solarsystem.adapters.CelestialObjectAdapter;
 import com.jsvoid.solarsystem.models.CelestialObject;
 import com.jsvoid.solarsystem.services.CelestialObjectResource;
 
@@ -27,6 +29,8 @@ import retrofit2.Retrofit;
 public class ListActivity extends AppCompatActivity {
 
     private Retrofit retrofit;
+    @Nullable
+    private CelestialObjectAdapter mCelestialObjectAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,6 +42,14 @@ public class ListActivity extends AppCompatActivity {
             toolbar.setTitle(R.string.app_name);
             setSupportActionBar(toolbar);
         }
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view_celestial_objects);
+        if (recyclerView != null) {
+            mCelestialObjectAdapter = new CelestialObjectAdapter(null);
+            recyclerView.setAdapter(mCelestialObjectAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        }
+
         setup();
         fetch();
     }
@@ -48,10 +60,10 @@ public class ListActivity extends AppCompatActivity {
         call.enqueue(new Callback<List<CelestialObject>>() {
             @Override
             public void onResponse(Call<List<CelestialObject>> call, Response<List<CelestialObject>> response) {
-                if (response.isSuccessful()) {
-                    Log.i("yay", "fetched");
+                if (response.isSuccessful() && mCelestialObjectAdapter != null) {
+                    mCelestialObjectAdapter.update(response.body());
                 } else {
-                    Log.i("fuck", "fucked");
+                    Toast.makeText(getApplicationContext(), R.string.error_fetch_objects, Toast.LENGTH_SHORT).show();
                 }
             }
 
@@ -63,6 +75,6 @@ public class ListActivity extends AppCompatActivity {
     }
 
     private void setup() {
-        retrofit = ((SolarSystemApplication)getApplication()).getRetrofit();
+        retrofit = ((SolarSystemApplication) getApplication()).getRetrofit();
     }
 }
--- app/src/main/java/com/jsvoid/solarsystem/adapters/CelestialObjectAdapter.java
+++ app/src/main/java/com/jsvoid/solarsystem/adapters/CelestialObjectAdapter.java
@@ -1,9 +1,13 @@
 package com.jsvoid.solarsystem.adapters;
 
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.jsvoid.solarsystem.R;
 import com.jsvoid.solarsystem.models.CelestialObject;
 
 import java.util.List;
@@ -15,31 +19,92 @@ import java.util.List;
  */
 public class CelestialObjectAdapter extends RecyclerView.Adapter<CelestialObjectAdapter.CelestialObjectViewHolder> {
 
-    private final List<CelestialObject> objects;
+    @Nullable
+    LayoutInflater inflater;
+    @Nullable
+    private Listener listener;
+    @Nullable
+    private List<CelestialObject> objects;
 
-    public CelestialObjectAdapter(List<CelestialObject> objects) {
+    public CelestialObjectAdapter(@Nullable List<CelestialObject> objects) {
         this.objects = objects;
     }
 
     @Override
     public CelestialObjectViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        if (inflater == null) {
+            inflater = LayoutInflater.from(parent.getContext());
+        }
+
+        View view = inflater.inflate(R.layout.celestial_object_list_item, parent, false);
+        return new CelestialObjectViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(CelestialObjectViewHolder holder, int position) {
-
+        if (objects != null) {
+            holder.update(objects.get(position), position);
+        }
     }
 
     @Override
     public int getItemCount() {
+        if (objects == null) {
+            return 0;
+        }
         return objects.size();
+    }
+
+    public Listener getListener() {
+        return listener;
+    }
+
+    public void setListener(@Nullable Listener listener) {
+        this.listener = listener;
+    }
+
+    public void update(List<CelestialObject> objects) {
+        this.objects = objects;
+        notifyDataSetChanged();
+    }
+
+    public interface Listener {
+        void onSelect(CelestialObject object);
     }
 
     public class CelestialObjectViewHolder extends RecyclerView.ViewHolder {
 
+        private int itemPosition;
+        private TextView mName;
+
         public CelestialObjectViewHolder(View itemView) {
             super(itemView);
+            setup();
+        }
+
+        private void setup() {
+            mName = (TextView) itemView.findViewById(R.id.object_name);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null && objects != null) {
+                        listener.onSelect(objects.get(getItemPosition()));
+                    }
+                }
+            });
+        }
+
+        public void update(CelestialObject object, int position) {
+            mName.setText(object.name);
+            setItemPosition(position);
+        }
+
+        public int getItemPosition() {
+            return itemPosition;
+        }
+
+        public void setItemPosition(int position) {
+            this.itemPosition = position;
         }
     }
 }
