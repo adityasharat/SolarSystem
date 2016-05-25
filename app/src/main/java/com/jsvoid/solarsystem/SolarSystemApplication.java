1643da6a832a3cf7e5cadfdf53f0e7552cafdcc7
Aditya Sharat <aditya.sharat@flipkart.com>
2016-05-25 13:27:23 +0530

    finalize changes

--- app/src/main/java/com/jsvoid/solarsystem/SolarSystemApplication.java
+++ app/src/main/java/com/jsvoid/solarsystem/SolarSystemApplication.java
@@ -12,7 +12,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class SolarSystemApplication extends Application {
 
-    private static final String BASE_URL = "http://10.0.2.2:8080";
+    private static final String BASE_URL = "http://jsvoid.com/files/solarsystem/";
 
     private Retrofit retrofit;
 
